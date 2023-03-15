23d05de3151a1c730aa1604145b5c53dd5401b7d
Byungjin-Kim <composerjins@gmail.com>
2023-03-15 11:30:58 -0500

    finished MVP

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { connect } from "react-redux";
+
 import Gif from "./Gif";
 
-import { connect } from "react-redux";
 
 const GifList = props => {
     const { gifs } = props;
